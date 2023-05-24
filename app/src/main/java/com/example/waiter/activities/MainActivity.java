843548cb52a16004c3144f0f442ce4f2ba0084db
Bakarijack <bakarikilu254@gmail.com>
2023-05-24 17:01:52 +0300

    removing the action bar from the mainActivity

--- app/src/main/java/com/example/waiter/activities/MainActivity.java
+++ app/src/main/java/com/example/waiter/activities/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.waiter.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
 
 import com.example.waiter.R;
 
@@ -11,6 +12,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //        get action bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+
         setContentView(R.layout.activity_main);
+
+
     }
 }
