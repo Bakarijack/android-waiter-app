4b4dc7e46e49d9b58c398c60ee64f5a173cc7397
Bakarijack <bakarikilu254@gmail.com>
2023-05-24 17:06:25 +0300

    making mainActivity fullscreen

--- app/src/main/java/com/example/waiter/activities/MainActivity.java
+++ app/src/main/java/com/example/waiter/activities/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.Window;
+import android.view.WindowManager;
 
 import com.example.waiter.R;
 
@@ -16,6 +17,9 @@ public class MainActivity extends AppCompatActivity {
         //        get action bar
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getSupportActionBar().hide();
+
+        //  set fullscreen
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_main);
 
